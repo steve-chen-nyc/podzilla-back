a9a6d8d03757a73d3241a86efb45f9bf1917645e
steve-chen-nyc <steve@Steves-MacBook-Pro.local>
2016-01-25 20:04:26 -0500

    updated api call for request

--- controllers/podcasts_controller.js
+++ controllers/podcasts_controller.js
@@ -6,53 +6,68 @@ const request = require('request');
 
 router.route('/business')
   .get((req, res) => {
-    appleClient('https://itunes.apple.com/search?term=business&entity=podcast&limit=50', function(body) {
+    request('https://itunes.apple.com/search?term=business&entity=podcast&limit=50', function(error, response,body) {
+      if(error) throw error;
+      if(response.statusCode !== 200){
+        res.json({message: 'Bad Status Code', response.statusCode});
+      }
       res.send(body)
   });
 });
 
 router.route('/comedy')
   .get((req, res) => {
-    appleClient('https://itunes.apple.com/search?term=comedy&entity=podcast&limit=50', function(body) {
+    request('https://itunes.apple.com/search?term=comedy&entity=podcast&limit=50', function(error, response, body) {
+      if(error) throw error;
+      if(response.statusCode !== 200){
+        res.json({message: 'Bad Status Code', response.statusCode});
+      }
       res.send(body)
   });
 });
 
 router.route('/technology')
   .get((req, res) => {
-    appleClient('https://itunes.apple.com/search?term=technology&entity=podcast&limit=50', function(body) {
+    request('https://itunes.apple.com/search?term=technology&entity=podcast&limit=50', function(error, response, body) {
+      if(error) throw error;
+      if(response.statusCode !== 200){
+        res.json({message: 'Bad Status Code', response.statusCode});
+      }
       res.send(body)
   });
 });
 
 router.route('/feelinglucky')
   .get((req, res) => {
-    appleClient('https://itunes.apple.com/search?term=podcast&entity=podcast&limit=50', function(body) {
+    request('https://itunes.apple.com/search?term=podcast&entity=podcast&limit=50', function(error, response, body) {
+      if(error) throw error;
+      if(response.statusCode !== 200){
+        res.json({message: 'Bad Status Code', response.statusCode});
+      }
       res.send(body)
   });
 });
 
 router.route('/ted')
   .get((req, res) => {
-    appleClient('https://itunes.apple.com/search?term=TED&entity=podcast&limit=50', function(body) {
+    request('https://itunes.apple.com/search?term=TED&entity=podcast&limit=50', function(error, response, body) {
+      if(error) throw error;
+      if(response.statusCode !== 200){
+        res.json({message: 'Bad Status Code', response.statusCode});
+      }
       res.send(body)
   });
 });
 
 router.route('/sports')
   .get((req, res) => {
-    appleClient('https://itunes.apple.com/search?term=sports&entity=podcast&limit=50', function(body) {
+    request('https://itunes.apple.com/search?term=sports&entity=podcast&limit=50', function(error, response ,body) {
+      if(error) throw error;
+      if(response.statusCode !== 200){
+        res.json({message: 'Bad Status Code', response.statusCode});
+      }
       res.send(body)
   });
 });
-
-
-  let appleClient = function(url,callback) {
-    request(url, function(err, res, body){
-      if (!err && res.statusCode == 200) {
-        callback(JSON.parse(body))
-      }
-    })
-  }
 
 module.exports = router;
